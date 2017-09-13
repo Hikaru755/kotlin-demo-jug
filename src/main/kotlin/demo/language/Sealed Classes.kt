c5f13d18d7f888453dc14fc9f9e837761d9d141e
Robin Roschlau <robin.roschlau@postadress.de>
2017-09-13 13:55:08 +0200

    :art: Improve sealed classes

--- src/main/kotlin/demo/language/Sealed Classes.kt
+++ src/main/kotlin/demo/language/Sealed Classes.kt
@@ -1,23 +1,23 @@
 package demo.language
 
 
-abstract class Result<out T>
-class Success<out T>(val value: T) : Result<T>()
-class Failure : Result<Nothing>()
+enum class Result {
+    Success, Failure
+}
 
 
-fun webcall(): Result<String> = TODO()
+fun webcall(): Result = TODO()
 
 
 fun main(args: Array<String>) {
 
     val result = webcall()
 
-//    val display: String = when (result) {
-//        is Success -> result.value
-//        is Failure -> "Error"
-//    }
-//
-//    println(display)
+    val display: String = when (result) {
+        Result.Success -> "Success"
+        Result.Failure -> "Error"
+    }
+
+    println(display)
 
 }
